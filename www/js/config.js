d836bb17be69da9bd7e621202347871319d56f6d
SupeRodv2 <supgeek.rod@gmail.com>
2016-05-20 11:48:40 +0800

    Revert "fix config cnd_domain and analytics code"
    This reverts commit 1e84410b747a727359b279790e0686f0d0fb04b0.

--- www/js/config.js
+++ www/js/config.js
@@ -7,7 +7,7 @@
 API_APP             =   'http://demo.hey-community.cn/api';             // app use the api
 API_WEBAPP          =   '';                                             // webApp use the api
 
-CDN_DOMAIN          =   'please use qiniu.com';                         // the cdn, app env is need
+CDN_DOMAIN          =   'http://public.hey-community.cn';               // the cdn, app env is need
 
 
 
