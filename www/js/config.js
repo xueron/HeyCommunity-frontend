72de574f18866b397361b29578df1a62d06b461f
SupeRodv2 <supgeek.rod@gmail.com>
2016-05-28 16:53:26 +0800

    fix config.js for webApp branch

--- www/js/config.js
+++ www/js/config.js
@@ -4,39 +4,19 @@
  * see doc:
  */
 
-API_APP             =   'http://demo.hey-community.cn/api';             // app use the api
-API_WEBAPP          =   '';                                             // webApp use the api
-
-CDN_DOMAIN          =   '';                                             // the cdn, app env is need
 
 
-
-
-
-
-
-
-
+//
 // auto set API
 if (window.location.protocol == 'http:' || window.location.protocol == 'https:') {
-    // set API
-    if (API_WEBAPP.substring(0, 4) == 'http') {
-        API     =   API_WEBAPP;
-    } else {
-        API     =   window.location.protocol + '//' + window.location.hostname + '/api';
-    }
-
-
-    // set CDN
-    if (getParameterByName('env') === undefined && CDN_DOMAIN.substring(0, 4) == 'http') {
-        CDN =   CDN_DOMAIN;
-    } else {
-        CDN =   window.location.protocol + '//' + window.location.hostname;
-    }
+    API     =   window.location.protocol + '//' + window.location.hostname + '/api';
+    CDN     =   window.location.protocol + '//' + window.location.hostname;
 } else {
-    API     =   API_APP;
-    CDN     =   CDN_DOMAIN;
+    alert('Please use the browser to open WEBAPP');
+    alert('Please use the browser to open WEBAPP');
+    alert('Please use the browser to open WEBAPP');
 }
+
 
 console.debug('the API is: ' + API);
 console.debug('the CDN is: ' + CDN);
